1a51e5b023e50bf94125ba0456acd39ac49b0643
wupc <sky@wupc.com>
2020-12-06 18:31:27 +0800

    第一次提交项目

---
+++ ssm_dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -0,0 +1,22 @@
+package com.itheima.dao;
+
+import com.itheima.bean.Items;
+
+import java.util.List;
+
+public interface ItemsDao {
+
+
+    /***
+     * 查询所有
+     * @return
+     */
+    List<Items> findAll();
+
+    /***
+     * 保存操作
+     * @param items
+     * @return
+     */
+    int save(Items items);
+}
